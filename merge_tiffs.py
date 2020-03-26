bd87e64d64fb7a0022e174c1a386e991f6dd6aa3
s1434165 <s1434165@ed.ac.uk>
2020-03-26 12:34:02 +0000

    Adding subsetting of .tifs to try and overcome memory problem using rasterio

--- merge_tiffs.py
+++ merge_tiffs.py
@@ -2,8 +2,9 @@
 import argparse
 import os
 import time
-import glob 
-import rasterio 
+import glob
+import numpy
+import rasterio
 from rasterio.merge import merge
 
 
@@ -13,7 +14,7 @@ def readCommands():
   """
   p = argparse.ArgumentParser(description=("Merging multiple tifs."))
   p.add_argument("--tifDir", dest ="tifDir", type=str, default='./', help=("Directory containing .tif files to merge"))
-  p.add_argument("--out_tif", dest ="out_tif", type=str, default='./output_merged.tif', help=("Output file path and name"))
+  p.add_argument("--chunks", dest ="chunks", type=int, default=2, help=("Number of files to mosaic iteratively"))
   cmdargs = p.parse_args()
   return cmdargs
 
@@ -25,10 +26,19 @@ class mergeTiffs(object):
         and merging them 
         into one
     """
-    def __init__(self,tifDir,out_tif):
-        self.multMerge(tifDir,out_tif)
+    def __init__(self,tifDir,chunks):
+        self.takeTifs(tifDir,chunks)
+
+    def chunkArray(self,array,chunk_size):
+        """
+            Using list comprehension
+            to break down an array 
+            into chunk sizes of choice
+        """ 
+        chunked_array = [array[i * chunk_size:(i + 1) * chunk_size] for i in range((len(array) + chunk_size - 1) // chunk_size )]  
+        return chunked_array
     
-    def multMerge(self,tifDir,out_tif):
+    def takeTifs(self,tifDir,chunks):
         """
             Function to search directory
             for all relevant files
@@ -43,34 +53,50 @@ class mergeTiffs(object):
 
         # opening these files and creating list of them (for merge)
         self.tifs_4_mosaic = []
+        
         for fp in tif_fps:
             print('Taking file ',fp,' for merge.')
-            src = rasterio.open(fp)
-            self.tifs_4_mosaic.append(src)
+            self.src = rasterio.open(fp)
+            self.tifs_4_mosaic.append(self.src)
 
-        # merging them into single array 
-        self.dest, self.out_trans = rasterio.merge.merge(self.tifs_4_mosaic)
+        self.tifs_4_mosaic_chunked = self.chunkArray(self.tifs_4_mosaic,chunks)
 
-        # updating metadata
+        self.mergeTifs()
+
+    def mergeTifs(self):
         """
-        out_meta = src.meta.copy()
-        out_meta.update({"driver": "Gtiff",
-                        "height": dest.shape[1],
-                        "width": dest.shape[2],
-                        "transform": out_trans})
+            Looping through chunks of tif
+            list to iteratively merge them
         """
+        i = 0
+        for chunk in self.tifs_4_mosaic_chunked:
+            # merging each file within the chunks them into single array 
+            print('Completing merge of chunk #',i+1,' of .tif list')
+            dest, out_trans = rasterio.merge.merge(chunk)
+
+            # output filename
+            out_tif = '../../HRSL/tiff_unzip/merged/merged_output_'+str(i)+'.tif'
+
+            # updating metadata
+            out_meta = self.src.meta.copy()
+            print('Updating metadata of outfile: ',out_tif)
+            out_meta.update({"driver": "Gtiff",
+                            "height": dest.shape[1],
+                            "width": dest.shape[2],
+                            "transform": out_trans})
+            
+            out_tif = '../../HRSL/tiff_unzip/merged/merged_output_'+str(i)+'.tif'
+            # writing merged file
+            with rasterio.open(out_tif, "w", **out_meta) as dest_obj:
+                    print('--Writing out merged geotiff--')
+                    dest_obj.write(dest)
+            i += 1
         
-        # writing merged file
-        """
-        with rasterio.open(out_tif, "w", **out_meta) as dest_obj:
-                print('--Writing out merged geotiff--')
-                dest_obj.write(dest)
-        """
 
 if __name__=="__main__":
     start_time = time.time()
     cmd = readCommands()
 
-    merged = mergeTiffs(tifDir='../../HRSL/tiff_unzip/',out_tif='../../HRSL/tiff_unzip/hrsl_global_pop_density.tif')
+    merged = mergeTiffs(tifDir='../../HRSL/tiff_unzip/',chunks=cmd.chunks)
 
     print("--- %s seconds ---" % (time.time() - start_time))
